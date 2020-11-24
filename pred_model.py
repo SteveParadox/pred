0e3f7fe362ee2d9979f0d134eb6a069ec0dc9429
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-11-24 12:10:55 +0100

    Add files via upload

--- pred_model.py
+++ pred_model.py
@@ -6,27 +6,49 @@ import random
 
 class Prediction:
     link = []
+    x = []
+    v = []
     p = range(1, 15)
     for a in p:
-        url = requests.get(f'https://livescore-api.com/api-client/fixtures/matches.json?&key=ZrrUvfcMuMdveWgO&secret=UDjsfGkjTwfY7h56Ix1dNd0h0eMea335+{a}')
-        link.append(url)
+        url = requests.get(
+            f'https://livescore-api.com/api-client/fixtures/matches.json?&key=ZrrUvfcMuMdveWgO&secret'
+            f'=UDjsfGkjTwfY7h56Ix1dNd0h0eMea335+{a}')
+        link.append(url.text)
 
     def __init__(self):
         self.__link = Prediction.link
+        self.__x = Prediction.x
+        self.__v = Prediction.v
 
     def model(self):
-        url = requests.get(
-            f'https://livescore-api.com/api-client/fixtures/matches.json?&key=ZrrUvfcMuMdveWgO&secret=UDjsfGkjTwfY7h56Ix1dNd0h0eMea335')
-        link= json.loads(url.text)
-        link.pop('success')
-        for i, e in link.items():
-            v = []
-            e.pop('prev_page')
-            e.pop('next_page')
-            for w, x in e.items():
-                for o in x:
-                    f = [n for i, n in o['competition'].items()]
-                    v.append(o['home_name'] + ' vs ' + o['away_name'] + ' || ' + f[-1] + ' || ' + o['time'] + ' || ' + o['date'])
+        for nr, url in enumerate(self.__link):
+            game = json.loads(url)
+            game.pop('success')
+            self.__x.append(game)
+
+            for i in self.__x:
+                o = i['data']
+                d = o['fixtures']
+
+                for p in d:
+                    f = [n for e, n in p['competition'].items()]
+                    games = p['home_name'] + ' vs ' + p['away_name'] + ' || ' + f[-1] + ' || ' + p['time'] + ' || ' + p[
+                        'date']
+
+                    self.__v.append(games)
+                    random.shuffle(self.__v)
+        data = random.sample(self.__v, 100)
+
+        random.shuffle(data)
+        choice = random.sample(data, 12)
+        l = []
+        for a in choice:
+            l.append(a)
+        with open('results.txt', 'w', encoding="utf-8") as r:
+            for i in l:
+                r.write(i + '\n')
 
 
-        pass
+c = Prediction()
+
+print(c.model())
